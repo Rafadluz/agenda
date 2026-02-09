485a16a3ff1ff45ef8d43b92ed39aa6244d0c716
Rafadluz <rafaela.lluzsantos@gmail.com>
2026-02-09 10:40:43 -0300

    refact: declarando explicitamente os tipos

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-
 from models.tarefa import Tarefa
 
 app = Flask(__name__)
@@ -10,15 +9,21 @@ def home():
 
 @app.route('/agenda', methods=['GET', 'POST'])
 def agenda():
-    tarefa = None
+    tarefas = None
     
     if request.method == 'POST':
         titulo_tarefa = request.form['titulo-tarefa']
         data_conclusao = request.form['data-conclusao']
         tarefa = Tarefa(titulo_tarefa, data_conclusao)
         tarefa.salvar_tarefa()
+    
+    tarefas = Tarefa.obter_tarefas ()
+    return render_template('agenda.html', titulo='Agenda', tarefas=tarefas)
 
-    return render_template('agenda.html', titulo='Agenda', tarefa=tarefa)
+@app.route('/delete/<int:idTarefa>')
+def delete(idTarefa):
+    tarefa = Tarefa.id(idTarefa)
+    print(tarefa)
 
 @app.route('/ola')
 def ola_mundo():
--- models/tarefa.py
+++ models/tarefa.py
@@ -1,13 +1,47 @@
+from sqlite3 import Cursor
 from models.database import Database
-from typing import Self
+from typing import Any, Self
 
 class Tarefa:
-    def __init__(self: Self, titulo_tarefa: str, data_conclusao: str = None)-> None:
+    def __init__(self: Self, titulo_tarefa: str, data_conclusao: str = None, id_tarefa: int = None)-> None:
         self.titulo_tarefa: str = titulo_tarefa
         self.data_conclusao: str = data_conclusao
+        self.id_tarefa: int = id_tarefa
+
+    #Tarefa(titulo_tarefa="Nova tarefa")
+    #Tarefa
+
+
+    @classmethod
+    def id(cls, id: int):
+        with Database('./data/tarefas.sqlite3') as db:
+            query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;'
+            params: tuple = (id,)
+            resultado = db.buscar_tudo(query,params)
+
+            #Desempacotamento de coleção
+            titulo,data = resultado
+
+        return cls(id_tarefa=id, titulo_tarefa = titulo, data_conclusao = data)
 
     def salvar_tarefa(self: Self) -> None:
         with Database('./data/tarefas.sqlite3') as db:
             query: str = "INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
             params: tuple = (self.titulo_tarefa, self.data_conclusao)
             db.executar(query, params)
+
+    @staticmethod
+    def obter_tarefas() -> list[Self]:
+        with Database('./data/tarefas.sqlite3')as db:
+            query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas;'
+            resultado: list[Any] = db.buscar_tudo(query)
+            tarefas: list[Self] = [Tarefa(titulo, data) for titulo, data in resultado]
+            #[[titulo, data]]
+            return tarefas
+        
+    def excluir_tarefa(self) -> Cursor:
+        with Database('./data/tarefas.sqlite3') as db:
+            query: str = 'DELETE FROM tarefas WHERE id = ?'
+            params: tuple = (self.id_tarefa,)
+            resultado: Cursor = db.executar(query, params)
+            return resultado
