41ce229b2300f2ce5a0de0e65540811f76402444
Rafaela <rafaela.lluzsantos@gmail.com>
2026-02-18 23:52:26 +0000

    Feat: adicionando funcionalidade de concluir tarefas.

--- models/tarefa.py
+++ models/tarefa.py
@@ -55,3 +55,10 @@ class Tarefa:
             params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa)
             resultado: Cursor = db.executar(query, params)
             return resultado
+
+    def concluir_tarefa(self):
+        with Database() as db:
+            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ? WHERE id = ?;'
+            params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa)
+            resultado: Cursor = db.executar(query, params)
+            return resultado
